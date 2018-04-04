0503e283142e371ebdfa76c4d98cabd38e872650
jiahao.lai <jiahao.lai@yitu-inc.com>
2018-04-04 16:21:13 +0800

    the master branch modification

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ int main() {
     int a = 1;
     std::cout << "Hello, World!" << std::endl;
     std::cout << a << std::endl;
+    std::cout << "Hello, World!, another branch" << std::endl;
     return 0;
 }
