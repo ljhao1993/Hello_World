e7aa48dd89bd1a169b246fe7ff424942d0e5bf9d
jiahao.lai <jiahao.lai@yitu-inc.com>
2018-04-04 19:12:20 +0800

    mode main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,5 @@ int main() {
     int a = 1;
     std::cout << "Hello, World!" << std::endl;
     std::cout << a << std::endl;
-    std::cout << "Hello, World!, another branch" << std::endl;
-    std::cout << "we have a emergency" << std::endl;
     return 0;
 }
