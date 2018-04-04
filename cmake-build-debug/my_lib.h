364ce7de1615ba7e12425fcf9aa8b8178097b37a
jiahao.lai <jiahao.lai@yitu-inc.com>
2018-04-04 14:21:25 +0800

    my first git c++ project

---
+++ cmake-build-debug/my_lib.h
@@ -0,0 +1,13 @@
+//
+// Created by jhlai on 18-4-4.
+//
+
+#ifndef HELLO_WORLD_MY_LIB_H
+#define HELLO_WORLD_MY_LIB_H
+
+class game
+{
+
+};
+
+#endif //HELLO_WORLD_MY_LIB_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 int main() {
+    int a = 1;
     std::cout << "Hello, World!" << std::endl;
+    std::cout << a << std::endl;
     return 0;
 }
