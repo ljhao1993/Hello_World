f6daa6a6d83ccd47ef998ae256773696b74007b9
jiahao.lai <jiahao.lai@yitu-inc.com>
2018-04-04 16:54:27 +0800

    an issue on the new branch

--- main.cpp
+++ main.cpp
@@ -4,5 +4,7 @@ int main() {
     int a = 1;
     std::cout << "Hello, World!" << std::endl;
     std::cout << a << std::endl;
+    std::cout << "try Hello, World!" << std::endl;
+    std::cout << "We have a issue here" << std::endl;
     return 0;
 }
